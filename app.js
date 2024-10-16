67d9ae4927daad983ff50e4e344175dfaed2da0a
~D.t <precede737@gmail.com>
2024-10-16 12:37:32 +0300

    adding new features
    html css js update on new features
    final player obj population

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ ball.addEventListener('click', () => {
    });
 */
 
+// console.log(obj);
 // let queryDelimeter = "?";
 // let parameterSeparator = "&";
 // let oid = "-25468809";
@@ -122,143 +123,142 @@ let rawObj = JSON.stringify([
     {
         "title": "Хоббит",
         "src": "9.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-110645251&id=456239468&hash=76faa5c1c1761eae"
     },
     {
         "title": "Впритык",
         "src": "10.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-220018529&id=456239739&hash=61bcbce72f9ff234"
     },
     {
         "title": "Пипец 2",
         "src": "11.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-121910473&id=456239820&hash=a1c87b6d9e2e4602"
     },
     {
         "title": "Эмоджи ",
         "src": "12.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-170685099&id=456240825&hash=982ac06eb500a62f"
     },
     {
         "title": "Зло возвращается",
         "src": "13.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-83600764&id=456244113&hash=3a4150982bf4053d"
     },
     {
         "title": "Тропы славы",
         "src": "14.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-176189699&id=456239048&hash=423f4004d90323da"
     },
     {
         "title": "Лего Бэтмен",
         "src": "15.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=465765428&id=456239531&hash=ead0c642ee7ff11f"
     },
     {
         "title": "1920 Лондон",
         "src": "16.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-31106704&id=456239129&hash=0f296767f2e599b1"
     },
     {
         "title": "АКВАМЕН ",
         "src": "17.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-176294899&id=456247886&hash=43c8c58b07c6806a"
     },
     {
         "title": "Человек-паук",
         "src": "19.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-217081635&id=456240066&hash=f8f163dfbfd8fdd1"
     },
     {
         "title": "Тьма",
         "src": "f-t-2.png",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-169276975&id=456242351&hash=1b97ab40c127cfed"
     },
     {
         "title": "Терминатор Зеро",
         "src": "s1.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-206317404&id=456239368&hash=622ed7a91ef977c7"
     },
     {
         "title": "Синяя тюрьма",
         "src": "s2.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-204399920&id=456241280&hash=662d056b7f90d602"
     },
     {
         "title": "Текен",
         "src": "s3.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-189724571&id=456254496&hash=33d74327ec131382"
     },
     {
         "title": "Киберпанк",
         "src": "s4.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-43444&id=456239748&hash=cdb6be7dbc0145fe"
     },
     {
         "title": "Пузырь ",
         "src": "s5.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-207691718&id=456246039&hash=db9e4abdbd3a6942"
     },
     {
         "title": "Летнее время",
         "src": "s6.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-185087421&id=456240709&hash=33b516e2e6977926"
     },
     {
         "title": "Суперзлодеи",
         "src": "s7.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-34748216&id=456260855&hash=cc67031728e5631b"
     },
     {
         "title": "Ая и ведьма",
         "src": "s8.jpg",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-207691718&id=456244818&hash=7f795fc8aba29e04"
     },
     {
         "title": "Игра престолов",
         "src": "a1.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-111951438&id=456242802&hash=96cc0b9ee098cf24"
     },
     {
         "title": "Во все тяжкие",
         "src": "a2.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=431766008&id=456241070&hash=0c3e91695d47bf03"
     },
     {
         "title": "Сопрано",
         "src": "a3.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-226750210&id=456239200&hash=568c750fa48566d2"
     },
     {
         "title": "Друзья ",
         "src": "a4.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-212294367&id=456241577&hash=94628a835114943f"
     },
     {
         "title": "Звоните Солу",
         "src": "a5.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=795782575&id=456239086&hash=db14110cccfb3e36"
     },
     {
         "title": "Офис ",
         "src": "a6.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-80021931&id=456244900&hash=30a9f8cede0cbaff"
     },
     {
         "title": "Тед Лассо",
         "src": "a7.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=818433616&id=456239061&hash=aca23828907f535e"
     },
     {
         "title": "Доктор Хаус",
         "src": "a8.webp",
-        "videoUrl": ""
+        "videoUrl": "https://vk.com/video_ext.php?oid=-80021931&id=456244812&hash=562f767a94f79330"
     }
 ]);
 let obj = JSON.parse(rawObj);
 let body = document.querySelector('body');
 let overlay = document.querySelector('.darken');
-
 
 
 function openPlayer(event){
@@ -304,7 +304,6 @@ async function playerFullscreen(){
     
     await player.requestFullscreen();
     
-    ////Screen Wake Lock API
     if ('wakeLock' in navigator) {
         try {
             const wakeLock = await navigator.wakeLock.request('screen');
@@ -313,9 +312,7 @@ async function playerFullscreen(){
             console.error(`Failed to acquire wake lock: ${err.name}, ${err.message}`);
         }
     }
-
 }
-
 
 body.addEventListener('click', closePlayer);
 
